4c433c3a2546ec64946dad28056b29020e65e82f
kim serin <kimsealin@naver.com>
2022-05-08 01:15:38 +0900

    #Test. 상품등록 페이지 관리자&유저 접근 테스트

---
+++ shop/src/test/java/com/shop/controller/ItemControllerTest.java
@@ -0,0 +1,53 @@
+package com.shop.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    //관리자 계정일 경우 테스트
+    @Test
+    @DisplayName("상품 등록 페이지 권한 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void itemFormTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new")) //상품 등록에 get 요청
+                .andDo(print()) //요청과 응답 메세지 콘솔창 출력
+                .andExpect(status().isOk()); //응답상태코드 확인
+        /*
+            MockHttpServletResponse:
+           Status = 200
+            Error message = null
+         */
+    }
+
+    //유저일 경우 테스트
+    @Test
+    @DisplayName("상품 등록 페이지 일반 회원 접근 테스트")
+    @WithMockUser(username = "user", roles = "USER")
+    public void itemFormNotAdminTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+        /*
+            MockHttpServletResponse:
+           Status = 403
+            Error message = Forbidden
+         */
+    }
+}
