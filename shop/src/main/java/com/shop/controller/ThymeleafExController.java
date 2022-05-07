aecd4685159d02970a81f0e6693e9bf33c3fd018
kim serin <kimsealin@naver.com>
2022-05-07 16:37:37 +0900

    #3-2. thymeleaf th:if, th:unless

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -49,4 +49,24 @@ public class ThymeleafExController {
         model.addAttribute("itemDtoList", itemDtoList);
         return "thymeleafeEx/thymeleafEx03";
     }
+
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1;i<=10;i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafeEx/thymeleafEx04";
+    }
+
+
 }
