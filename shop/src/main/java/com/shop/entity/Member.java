69f25c9115bd22f306d856a84d0b2c91f01ffd5f
kim serin <kimsealin@naver.com>
2022-05-07 20:42:41 +0900

    #4-2. 비밀번호 암호화하여 로그인 기능 작성

---
+++ shop/src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,50 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member")
+@Getter @Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //Member 엔티티를 생성하는 메소드. 회원을 생성하는 메서드를 만들어서 관리하면 코드가 변경되더라도 한 군데만 수정하면 된다.
+    public static Member createMember(MemberFormDto memberFormDto,
+                                      PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+
+        //시큐리티 설정에 등록한 BCryptPasswordEncoder Bean을 파라미터로 넘겨 받아서 암호화된 비밀번호를 저장한다.
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+
+        member.setRole(Role.USER);
+        return member;
+    }
+}
---
+++ shop/src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    //@RequiredArgsContructor 을 사용하여 final이나 @NonNull이 붙은 필드에 생성자를 생성해준다.
+    //빈을 주입하는 @Autowired의 다른 방법
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member){ //이미 가입된 회원의 경우 IllegalStateException을 발생시킨다.
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null){
+            throw new IllegalStateException("이미 가입된 회원입니다");
+        }
+    }
+}
