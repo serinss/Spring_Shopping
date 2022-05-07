a4d238c022441727628e6cddf5571d99cbf9310c
kim serin <kimsealin@naver.com>
2022-05-07 21:31:36 +0900

    #4-4. 회원가입 View 출력

---
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -0,0 +1,51 @@
+package com.shop.controller;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    //Dto를 생성하고, 회원 가입 창으로 이동하기
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    //실제 회원가입을 진행하고 성공 시, 메인 화면으로 이동하기
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+    //검사 후 결과를 bindingResult에 담는다 -> hassError로 체크
+
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+}
