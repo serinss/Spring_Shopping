8e8be73f78584e5888bed00cf30a862ed1b1eb72
kim serin <kimsealin@naver.com>
2022-05-08 18:03:56 +0900

    #5-3. order 영속성 전이 타입 ALL 설정

--- shop/src/main/java/com/shop/entity/Order.java
+++ shop/src/main/java/com/shop/entity/Order.java
@@ -27,7 +27,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
-    @OneToMany(mappedBy = "order") // 주문 상품과 일대다 매핑 -> 외래키(order_id)가 OrderItem에 있으므로 연관관계의 주인은 OrderItem
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 주문 상품과 일대다 매핑 -> 외래키(order_id)가 OrderItem에 있으므로 연관관계의 주인은 OrderItem
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
