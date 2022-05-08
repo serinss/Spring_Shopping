b894df19e011d7a66e1ece116fe6aacf0fc98232
kim serin <kimsealin@naver.com>
2022-05-08 18:33:39 +0900

    #5-5. BaseEntity 상속

--- shop/src/main/java/com/shop/entity/OrderItem.java
+++ shop/src/main/java/com/shop/entity/OrderItem.java
@@ -4,11 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -24,6 +23,6 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
+    //private LocalDateTime regTime; 삭제, BaseEntity 상속
+    //private LocalDateTime updateTime;
 }
