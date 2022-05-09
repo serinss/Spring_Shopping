0854d9b9773c3d97264e731f7df3a5f816ea1a60
kim serin <kimsealin@naver.com>
2022-05-09 18:08:45 +0900

    #7-2. 제품 재고 Exception 체크

--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
 import com.shop.dto.ItemFormDto;
+import com.shop.exception.OutOfStockException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -46,5 +47,16 @@ public class Item extends BaseEntity{ //상품의 가장 기본적인 정보
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+    
+    public void removeStock(int stockNumber){
+        
+        // 주문 후 남을 재고의 수량 구하기
+        int restStock = this.stockNumber - stockNumber;
+        if(restStock<0){
+            throw new OutOfStockException("상품의 재고가 부족 합니다. (현재 재고 수량: "+this.stockNumber+")");
+        }
+        //주문 후 남은 재고 수량을 상품의 현재 재고 값으로 할당
+        this.stockNumber = restStock;
     }
 }
