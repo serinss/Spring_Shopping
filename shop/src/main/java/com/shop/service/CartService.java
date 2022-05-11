2030b9dc48457bcd831f10731534590641aaab2b
kim serin <kimsealin@naver.com>
2022-05-11 23:02:44 +0900

    #8-2. 장바구니 생성 Controller Service 매핑

--- shop/src/main/java/com/shop/service/CartService.java
+++ shop/src/main/java/com/shop/service/CartService.java
@@ -1,11 +1,56 @@
 package com.shop.service;
 
+import com.shop.dto.CartItemDto;
+import com.shop.entity.Cart;
+import com.shop.entity.CartItem;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import com.shop.repository.CartItemRepository;
+import com.shop.repository.CartRepository;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public Long addCart(CartItemDto cartItemDto, String email){
+        // 장바구니 담을 상품 엔티티 조회
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        // 현재 로그인한 회원 엔티티 조회
+        Member member = memberRepository.findByEmail(email);
+
+        // 로그인 한 아이디의 장바구니 엔티티 조회
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if(cart == null){ // 처음으로 장바구니 담을 경우 회원 장바구니 엔티티 생성
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        // 이미 장바구니에 있느 상품인지 조회
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+        if(savedCartItem != null){ // 이미 장바구니에 있는 경우 현재 장바구니에 담을 수량만큼 ++
+            savedCartItem.addCount(cartItemDto.getCount());
+
+            return savedCartItem.getId();
+        } else{
+            // 없는 경우 장바구니 엔티티, 상품 엔티티, 장바구니에 담을 수량을 이용하여 CartItem 엔티티 생성
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+
+            return cartItem.getId();
+        }
+    }
 }
