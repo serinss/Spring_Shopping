59a54be0f30aba7b2a297d12ff65f6ab68505024
kim serin <kimsealin@naver.com>
2022-05-11 23:03:10 +0900

    #Test. 장바구니 담기 테스트

---
+++ shop/src/test/java/com/shop/service/CartServiceTest.java
@@ -0,0 +1,78 @@
+package com.shop.service;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.CartItemDto;
+import com.shop.entity.CartItem;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import com.shop.repository.CartItemRepository;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class CartServiceTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    // 장바구니 상품과 회원 정보 저장
+    public Item saveItem(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember(){
+        Member member = new Member();
+        member.setEmail("test@test.com");
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    public void addCart(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        // 장바구니 개체 변수 저장
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setCount(5);
+        cartItemDto.setItemId(item.getId());
+
+        // 장바구니 담는 메서드 호출 결과 생성된 장바구니 상품 아이디를 cartItemId에 저장
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        // 상품 아이디와 장바구니에 저장된 상품 아이디가 같다면 테스트 종료
+        assertEquals(item.getId(), cartItem.getItem().getId());
+        assertEquals(cartItemDto.getCount(), cartItem.getCount());
+    }
+
+}
